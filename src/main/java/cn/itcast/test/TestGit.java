da5415ce05f496a67c6323c05cea5b33d7c13899
yaodx <yaodx126@126.com>
2017-06-23 15:09:10 +0800

    Remote File 222
    Remote File 222

--- src/main/java/cn/itcast/test/TestGit.java
+++ src/main/java/cn/itcast/test/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("init Git 000");
 		System.out.println("Git File 111");
+		System.out.println("Remote File 222");
 	}
 
 }
